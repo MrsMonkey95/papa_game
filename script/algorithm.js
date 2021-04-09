3da57afb32196ccef6d24cc5f1a688ac9d9ec548
MrsMonkey95 <rahel.engelmann@bwz-rappi.ch>
2021-04-09 15:39:19 +0200

    added more js

--- script/algorithm.js
+++ script/algorithm.js
@@ -1,11 +1,34 @@
+/* 
+Script for the game algorithm.
+created by MrsMonkey95 on 09.04.2021 
+*/
 
+// Variables
 let lastRenderTime = 0;
+import { PAPA_SPEED } from './papa.js';
 
+// Game loop
 function main(currentTime) {
-    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
     window.requestAnimationFrame(main);
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+    // Checking render time, how often rendering per second
+    if (secondsSinceLastRender < 1 / PAPA_SPEED) return;
+    
     lastRenderTime = currentTime;
-    console.log(secondsSinceLastRender);
+    console.log('Render');
+/* Game logic starting here */
+
+    update()
+
+    draw()
 }
 
 window.requestAnimationFrame(main);
+
+function update() {
+
+}
+
+function draw() {
+
+}
---
+++ script/papa.js
@@ -0,0 +1,7 @@
+/*
+    Logic for the papa snake
+    created by MrsMonkey95 on 09.04.2021
+*/
+
+export const PAPA_SPEED = 1;
+
