ebad52864a8fe533488aa54745911c4679728a54
MrsMonkey95 <rahel.engelmann@bwz-rappi.ch>
2021-04-10 19:37:55 +0200

    played around with sound

--- script/audio.js
+++ script/audio.js
@@ -2,3 +2,19 @@
     created by MrsMonkey95 on 10.04.2021
 */
 
+
+
+// var ragnarSong = new Audio('audio/Flymetothemoon-Ragnar.mp3');
+var coin = new Audio('./audio/coin.mp3');
+
+
+/* export function playRagnarSong(){
+    //ragnarSong.play();
+    new Audio('./audio/Flymetothemoon-Ragnar.mp3').play();
+} */
+
+// coin is working in edge but doesn't work in chrome...  -> fixed in chrome settings
+// TO DO: figure out how to fix this shit without turning off hardware acceleration in chrome manually
+export function playCoin(){
+    coin.play();
+}
