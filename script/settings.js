82f25518a02b3bc433cd1a55aeb3ed6e8e58fdb9
MrsMonkey95 <rahel.engelmann@bwz-rappi.ch>
2021-04-19 01:20:34 +0200

    important info added as comment

--- script/settings.js
+++ script/settings.js
@@ -3,6 +3,22 @@
     created by MrsMonkey95 on 18.04.2021
  */
 
+
+
+/* 
+    IMPORTANT NOTE: Radio buttons are still taking "keydown" inputs, so at the moment one must click on the page somewhere to use the arrow keys for the game,
+                    otherwise the arrow keys will switch through the radio buttons and cause a terrible mess...
+*/
+
+
+
+
+
+
+
+
+
+
      // fuck this I wrote it directly into algorithm.js  
 
      /* useless piece of code */
