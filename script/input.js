24a93105ac6cfa01bb03e37008615616be0e8c54
Stefan Bühler <stefan.buehler@bitsolutions.ch>
2021-05-06 16:18:11 +0200

    prevent default propagation of event

--- script/input.js
+++ script/input.js
@@ -9,18 +9,22 @@ let lastInputDirection = { x: 0, y: 0 };
 window.addEventListener('keydown', e => {
     switch (e.key) {
         case 'ArrowUp':
+            e.preventDefault();
             if (lastInputDirection.y !== 0) break;
             inputDirection = { x: 0, y: -1 };
             break;
         case 'ArrowDown':
+            e.preventDefault();
             if (lastInputDirection.y !== 0) break;
             inputDirection = { x: 0, y: 1 };
             break;
         case 'ArrowLeft':
+            e.preventDefault();
             if (lastInputDirection.x !== 0) break;
             inputDirection = { x: -1, y: 0 };
             break;
         case 'ArrowRight':
+            e.preventDefault();
             if (lastInputDirection.x !== 0) break;
             inputDirection = { x: 1, y: 0 };
             break;
